3c42c5e6b2aa3c6c306604bfb26ea93f2e1122ce
Renhai <rh_king@163.com>
2023-11-03 10:41:53 +0800

    [update]: add commends to guid enable vim smoothly and enable YCM plugin
    Signed-off-by: Renhai <rh_king@163.com>

--- install.sh
+++ install.sh
@@ -6,3 +6,7 @@ cp -rf vimrc ~/.vimrc
 sudo apt update
 sudo apt install cscope universal-ctags
 sudo apt install vim-python*
+
+# open vim and input :PlugInstall
+# Enter .vim/Plug/YouCompileMe and execute git submodule update --recursive --remote and ./install.sh --clang-completer
+# OK YCM fine.
